f4ab40439c5b2aac661adc80486aa185c56bfe87
Daiz Wap <zmatder@outlook.com>
2024-01-27 17:25:16 +0100

    add login feature

--- AchiveAPP/Form1.Designer.cs
+++ AchiveAPP/Form1.Designer.cs
@@ -29,30 +29,76 @@
         private void InitializeComponent()
         {
             button1 = new Button();
+            usernameBox = new TextBox();
+            passwordBox = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(347, 263);
+            button1.Location = new Point(389, 299);
             button1.Name = "button1";
-            button1.Size = new Size(274, 135);
+            button1.Size = new Size(274, 69);
             button1.TabIndex = 0;
-            button1.Text = "button1";
+            button1.Text = "log in";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // usernameBox
+            // 
+            usernameBox.Location = new Point(341, 88);
+            usernameBox.Name = "usernameBox";
+            usernameBox.Size = new Size(397, 39);
+            usernameBox.TabIndex = 1;
+            // 
+            // passwordBox
+            // 
+            passwordBox.Location = new Point(341, 206);
+            passwordBox.Name = "passwordBox";
+            passwordBox.Size = new Size(397, 39);
+            passwordBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(206, 88);
+            label1.Name = "label1";
+            label1.Size = new Size(119, 32);
+            label1.TabIndex = 3;
+            label1.Text = "username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(206, 213);
+            label2.Name = "label2";
+            label2.Size = new Size(113, 32);
+            label2.TabIndex = 4;
+            label2.Text = "password";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1074, 660);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(passwordBox);
+            Controls.Add(usernameBox);
             Controls.Add(button1);
             Name = "Form1";
             Text = "Form1";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
         private Button button1;
+        private TextBox usernameBox;
+        private TextBox passwordBox;
+        private Label label1;
+        private Label label2;
     }
 }
--- AchiveAPP/Form1.cs
+++ AchiveAPP/Form1.cs
@@ -4,14 +4,51 @@ namespace AchiveAPP
     public partial class Form1 : Form
     {
         Connection connection = new Connection();
+        bool x = false;
         public Form1()
         {
             InitializeComponent();
             connection.Database.EnsureCreated();
-            connection.users.Add(new User());
+            if (x)
+            {
+                connection.users.Add(new User()
+                {
+                    Num_Telephone = "08030999490",
+                    FName = "Ahmed",
+                    
+                    LName = "Bennadji",
+                    Password = "123",
+                    Username = "123"
+                });
+                connection.SaveChanges();
+
+            }
+
+
+
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string user = usernameBox.Text;
+            string pass = passwordBox.Text;
+
+            var resutl = connection.users.Where(s => s.Username == user && s.Password == pass);
+
+            if (resutl.Count() == 0)
+                MessageBox.Show("the password is incorrect");
+            else if(resutl.Count()== 1)
+            {
+                var info = resutl.First();
+                MessageBox.Show("Welcome Mr." + info.LName);
+            }
 
 
             
+
         }
     }
 }
